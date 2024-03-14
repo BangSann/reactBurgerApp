495df746ebdc7dc2b12037eed0290c947c7a3a3d
BangSann <yosanokta12@gmail.com>
2024-03-14 22:50:07 +0700

    feat: Commit 5

--- src/component/button/index.jsx
+++ src/component/button/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Button = ({childern,link,variant="secondary"}) => {
     return (   
         <section>
-            <a href={link}>
+            <Link to={link}>
                 <button class={`p-2 rounded-lg w-40 border ${variant}`}>{childern}</button>
-            </a>
+            </Link>
+            
         </section>
       );
 }
